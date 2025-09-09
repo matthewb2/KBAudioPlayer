7060a9a3b5d632c5c5be722b6c4eb3ae380e0ac4
matthewb2 <bestsciencekwabe@gmail.com>
2025-09-09 14:01:23 +0900

    fixed error

---
+++ src/RealTimePlayback.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Dsp;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using System.Diagnostics;
+
+namespace KBAudioPlayer
+{
+    public class RealTimePlayback 
+    {
+        private WasapiCapture _capture;
+        private object _lock;
+        private int _fftPos;
+        private int _fftLength;
+        private Complex[] _fftBuffer;
+        private float[] _lastFftBuffer;
+        private float[] meters1=null;
+
+        private bool _fftBufferAvailable;
+        private int _m;
+        private Boolean isPlay = false;
+
+        public RealTimePlayback()
+        {
+            this._lock = new object();
+
+            this._capture = new WasapiLoopbackCapture();
+            this._capture.DataAvailable += this.DataAvailable;
+
+            this._m = (int)Math.Log(this._fftLength, 2.0);
+            this._fftLength = 2048; // 44.1kHz.
+            this._fftBuffer = new Complex[this._fftLength];
+            this._lastFftBuffer = new float[this._fftLength];
+            this.meters1 = new float[this._fftLength];
+        }
+
+        public WaveFormat Format
+        {
+            get
+            {
+                return this._capture.WaveFormat;
+            }
+        }
+
+        private float[] ConvertByteToFloat(byte[] array, int length)
+        {
+            int samplesNeeded = length / 4;
+            float[] floatArr = new float[samplesNeeded];
+
+            for (int i = 0; i < samplesNeeded; i++)
+            {
+                floatArr[i] = BitConverter.ToSingle(array, i * 4);
+            }
+
+            return floatArr;
+        }
+
+        private void DataAvailable(object sender, WaveInEventArgs e)
+        {
+            // Convert byte[] to float[].
+            float[] data = ConvertByteToFloat(e.Buffer, e.BytesRecorded);
+
+            // For all data. Skip right channel on stereo (i += this.Format.Channels).
+            for (int i = 0; i < data.Length; i += this.Format.Channels)
+            {
+                this._fftBuffer[_fftPos].X = (float)(data[i] * FastFourierTransform.HannWindow(_fftPos, _fftLength));
+                this._fftBuffer[_fftPos].Y = 0;
+                this._fftPos++;
+
+                if (this._fftPos >= this._fftLength)
+                {
+                    this._fftPos = 0;
+
+                    // NAudio FFT implementation.
+                    FastFourierTransform.FFT(true, this._m, this._fftBuffer);
+
+                    // Copy to buffer.
+                    lock (this._lock)
+                    {
+                        for (int c = 0; c < this._fftLength; c++)
+                        {
+                            this._lastFftBuffer[c] = this._fftBuffer[c].X;
+                            this.meters1[c] = this._fftBuffer[c].X;
+                        }
+
+                        this._fftBufferAvailable = true;
+                    }
+                }
+            }
+            //print out
+            for (int c = 0; c < this._fftLength; c++)
+            {
+                this._lastFftBuffer[c] = this._fftBuffer[c].X;
+                //Debug.Write(this._fftBuffer[c].X);
+                //Debug.Write(":");
+
+            }
+            //Debug.WriteLine("");
+            //
+            
+
+        }
+
+        public float[] GetFFTData()
+        {
+            return this.meters1;
+        }
+
+        public void Start()
+        {
+            this._capture.StartRecording();
+            this.isPlay = true;
+        }
+
+        public void Stop()
+        {
+            this._capture.StopRecording();
+            this.isPlay = false;
+        }
+
+        public bool GetFFTData(float[] fftDataBuffer)
+        {
+            lock (this._lock)
+            {
+                // Use last available buffer.
+                if (this._fftBufferAvailable)
+                {
+                    this._lastFftBuffer.CopyTo(fftDataBuffer, 0);
+                    this._fftBufferAvailable = false;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public int GetFFTFrequencyIndex(int frequency)
+        {
+            int index = (int)(frequency / (this.Format.SampleRate / this._fftLength / this.Format.Channels));
+            return index;
+        }
+
+        public bool IsPlaying()
+        {
+            return isPlay;
+        }
+        
+    }
+}
---
+++ src/System/Windows/Forms/ListViewWithoutScrollBar.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.InteropServices;
+using Deveck.Ui.Controls;
+
+namespace System.Windows.Forms
+{
+    internal class ListViewWithoutScrollBar : CustomListView
+    {
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x83: // WM_NCCALCSIZE
+                    int style = (int)GetWindowLong(this.Handle, GWL_STYLE);
+                    if ((style & WS_VSCROLL) == WS_VSCROLL)
+                        SetWindowLong(this.Handle, GWL_STYLE, style & ~WS_VSCROLL);
+                    //if ((style & WS_HSCROLL) == WS_HSCROLL)
+                    //    SetWindowLong(this.Handle, GWL_STYLE, style & ~WS_HSCROLL);
+                    base.WndProc(ref m);
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+        const int GWL_STYLE = -16;
+        const int WS_VSCROLL = 0x00200000;
+        const int WS_HSCROLL = 0x00100000;
+
+        public static int GetWindowLong(IntPtr hWnd, int nIndex)
+        {
+            if (IntPtr.Size == 4)
+                return (int)GetWindowLong32(hWnd, nIndex);
+            else
+                return (int)(long)GetWindowLongPtr64(hWnd, nIndex);
+        }
+
+        public static int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong)
+        {
+            if (IntPtr.Size == 4)
+                return (int)SetWindowLongPtr32(hWnd, nIndex, dwNewLong);
+            else
+                return (int)(long)SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
+        }
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetWindowLong32(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLongPtr", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetWindowLongPtr64(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong", CharSet = CharSet.Auto)]
+        public static extern IntPtr SetWindowLongPtr32(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", CharSet = CharSet.Auto)]
+        public static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int nIndex, int dwNewLong);
+    }
+}
